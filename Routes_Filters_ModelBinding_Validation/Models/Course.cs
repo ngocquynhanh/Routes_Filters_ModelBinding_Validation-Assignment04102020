5d33a729cbdcc95fe816d95140b122b2e4f15455
Anh Nguyen <ngocquynhanh@usf.edu>
2020-04-10 12:59:53 -0400

    Add project files.

---
+++ Routes_Filters_ModelBinding_Validation/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Routes_Filters_ModelBinding_Validation.Models
+{
+    public class Course
+    {
+        public string CourseName { get; set; }
+        public string BuildingCode { get; set; }
+        public string RoomNumber { get; set; }
+    }
+}
---
+++ Routes_Filters_ModelBinding_Validation/Models/PhoneNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Routes_Filters_ModelBinding_Validation.Models
+{
+    public class PhoneNumber
+    {
+        public class Phone
+        {
+            public string Type { get; set; }
+            public string PhoneNumber { get; set; }
+            public bool IsEvening { get; set; }
+        }
+    }
+}
